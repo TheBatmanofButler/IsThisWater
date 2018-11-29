d82bea18c721b2fd7a04dc89c7fe25e2d2deb743
buttermilkpopcorn <gr2483@columbia.edu>
2018-11-28 21:11:12 -0500

    create notebooks and Flask UI to generate dataset

--- app.py
+++ app.py
@@ -1,39 +1,68 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+
 import os
-import image
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
+MAPBOX_ACCESS_TOKEN = os.environ['MAPBOX_ACCESS_TOKEN']
+STANDARD_ZOOM = 15
+filepath = 'sites.json'
+
+with open(filepath) as fp:
+    sites_dict = json.load(fp)
+
+
+@app.route('/', methods=['GET'])
 def index():
-	 return render_template('index.html', data = {})
+    site = sites_dict['unchecked'][0]
+    lat = site.get('lat')
+    lon = site.get('lon')
+    num_images_labeled = len(sites_dict['water_found']) + len(sites_dict['water_not_found'])
 
-@app.route('/', methods=['POST'])
-def form_post():
+    print_image_log(site)
 
-    state_code = request.form['stateCode']
-    number_of_hits = int(request.form['numberOfHits'])
+    return render_template('index.html',
+                            num_images_labeled=num_images_labeled,
+                            lat=lat,
+                            lon=lon,
+                            zoom=STANDARD_ZOOM,
+                            MAPBOX_ACCESS_TOKEN=MAPBOX_ACCESS_TOKEN)
 
-    data = image.get_water_sources(state_code, number_of_hits)
 
-    return render_template('index.html', state_code = state_code,
-    					number_of_hits = number_of_hits,
-    					data = data
-    					)
+@app.route('/load_next_image', methods=['POST'])
+def load_next_image():
+    water_found, zoom = request.json
 
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
+    site = sites_dict['unchecked'].pop(0)
+    site['zoom'] = zoom
+    
+    if water_found:
+        sites_dict['water_found'].append(site)
+    else:
+        sites_dict['water_not_found'].append(site)
+    
+    with open(filepath, 'w+') as fp:
+        json.dump(sites_dict, fp)
 
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+    if not len(sites_dict['unchecked']):
+        return jsonify(None)
+
+    site = sites_dict['unchecked'][0]
+    lat = site['lat']
+    lon = site['lon']
+    zoom = STANDARD_ZOOM
+    num_images_labeled = len(sites_dict['water_found']) + len(sites_dict['water_not_found'])
+    print_image_log(site)
+
+    return jsonify([lat, lon, zoom, num_images_labeled])
+
+
+def print_image_log(site):
+    print(site.get('name'), site.get('site_code'))
+
 
 if __name__ == '__main__':
-	app.debug = True
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+    app.debug = False
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- static/main.js
+++ static/main.js
@@ -1,13 +1,62 @@
-$('.result-row').click(function () {
-//  $(this).child('.remove-box'). 
-  if ($(this).hasClass('selected')) {
-    $(this).removeClass('selected');
-    // $(this).css('background', 'red');
-  }
-  else {
-    $(this).addClass('selected');
-    // $(this).css('background', '#fff');
-  }
+let $image = $('.right-panel img'),
+
+    getImageSrc = function () {
+      return 'https://api.mapbox.com/v4/mapbox.satellite/' + 
+            lon + ',' + lat + ',' + zoom + '/1000x1000.png32?access_token='
+            + MAPBOX_ACCESS_TOKEN
+    },
+
+    updateZoom = function () {
+      $('#zoom-level').html(zoom);
+      $('#num-images-labeled').html(num_images_labeled);
+      $image.css('opacity', '0');
+      $image.attr('src', getImageSrc());
+      $image.on('load', null);
+      $image.on('load', function () {
+        $image.css('opacity', '1');
+      });
+    },
+
+    submitLabel = function (label) {
+      $.ajax({
+        url: '/load_next_image',
+        data: JSON.stringify([label, zoom]),
+        type: 'POST',
+        contentType: 'application/json',
+        success: function(response) {
+          if (!response) {
+            alert('All images checked.');
+            return;
+          }
+    
+          lat = response[0];
+          lon = response[1];
+          zoom = response[2];
+          num_images_labeled = response[3];
+          updateZoom();
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
+    }
+
+$('#zoom-in-button').click( function (e) {
+  zoom++;
+  updateZoom();
 });
 
-$("table").stickyTableHeaders();
+$('#zoom-out-button').click( function (e) {
+  if (zoom == 0) return;
+
+  zoom--;
+  updateZoom();
+});
+
+$('#no-button').click( function (e) {
+  submitLabel(false);
+});
+
+$('#yes-button').click( function (e) {
+  submitLabel(true);
+});
